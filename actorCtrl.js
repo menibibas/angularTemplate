b627708bd8e276b8688e9b10992486976c09d1ae
meni bibas <menibibas@gmail.com>
2019-06-13 14:54:33 +0300

    adding the selection and unselection

--- actorCtrl.js
+++ actorCtrl.js
@@ -1,4 +1,4 @@
-app.controller("actorCtrl", function ($scope) {
+app.controller("actorCtrl", function ($scope,$http) {
 
     //**** Business Logic - this will eventually move to service ****
     function Actor(fname, lname, image, birthday, imdb) {
@@ -14,14 +14,14 @@ app.controller("actorCtrl", function ($scope) {
     };
    
     // Load 6 actors
-    $scope.actors = [];
-    $scope.actors.push(new Actor("Bruce", "Willis", "https://m.media-amazon.com/images/M/MV5BMjA0MjMzMTE5OF5BMl5BanBnXkFtZTcwMzQ2ODE3Mw@@._V1_UY317_CR27,0,214,317_AL_.jpg", "1955-03-19", "https://www.imdb.com/name/nm0000246"));
-    $scope.actors.push(new Actor("Sharon", "Stone", "https://m.media-amazon.com/images/M/MV5BMTg0MDU1ODQwNF5BMl5BanBnXkFtZTcwOTc3MjQwNA@@._V1_UY317_CR4,0,214,317_AL_.jpg", "1958-03-10", "https://www.imdb.com/name/nm0000232"));
-    $scope.actors.push(new Actor("Michael", "Douglas", "https://m.media-amazon.com/images/M/MV5BMTQ3NzMzOTQ3MF5BMl5BanBnXkFtZTcwOTE0MzY1Mw@@._V1_UY317_CR13,0,214,317_AL_.jpg", "1944-09-25", "https://www.imdb.com/name/nm0000140"));
-    $scope.actors.push(new Actor("Ashton", "Kutcher", "https://m.media-amazon.com/images/M/MV5BMzIzMTE4NzcyMl5BMl5BanBnXkFtZTgwODA2NTQyNTM@._V1_UY317_CR131,0,214,317_AL_.jpg", "1978-02-07", "https://www.imdb.com/name/nm0005110"));
-    $scope.actors.push(new Actor("Jennifer", "Aniston", "https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg", "1969-02-11", "https://www.imdb.com/name/nm0000098"));
-    $scope.actors.push(new Actor("Scarlett", "Johansson", "https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_UY317_CR23,0,214,317_AL_.jpg", "1984-11-22", "https://www.imdb.com/name/nm0424060"));
-    console.log($scope.actors);
+    // $scope.actors = [];
+    // $scope.actors.push(new Actor("Bruce", "Willis", "https://m.media-amazon.com/images/M/MV5BMjA0MjMzMTE5OF5BMl5BanBnXkFtZTcwMzQ2ODE3Mw@@._V1_UY317_CR27,0,214,317_AL_.jpg", "1955-03-19", "https://www.imdb.com/name/nm0000246"));
+    // $scope.actors.push(new Actor("Sharon", "Stone", "https://m.media-amazon.com/images/M/MV5BMTg0MDU1ODQwNF5BMl5BanBnXkFtZTcwOTc3MjQwNA@@._V1_UY317_CR4,0,214,317_AL_.jpg", "1958-03-10", "https://www.imdb.com/name/nm0000232"));
+    // $scope.actors.push(new Actor("Michael", "Douglas", "https://m.media-amazon.com/images/M/MV5BMTQ3NzMzOTQ3MF5BMl5BanBnXkFtZTcwOTE0MzY1Mw@@._V1_UY317_CR13,0,214,317_AL_.jpg", "1944-09-25", "https://www.imdb.com/name/nm0000140"));
+    // $scope.actors.push(new Actor("Ashton", "Kutcher", "https://m.media-amazon.com/images/M/MV5BMzIzMTE4NzcyMl5BMl5BanBnXkFtZTgwODA2NTQyNTM@._V1_UY317_CR131,0,214,317_AL_.jpg", "1978-02-07", "https://www.imdb.com/name/nm0005110"));
+    // $scope.actors.push(new Actor("Jennifer", "Aniston", "https://m.media-amazon.com/images/M/MV5BNjk1MjIxNjUxNF5BMl5BanBnXkFtZTcwODk2NzM4Mg@@._V1_UY317_CR3,0,214,317_AL_.jpg", "1969-02-11", "https://www.imdb.com/name/nm0000098"));
+    // $scope.actors.push(new Actor("Scarlett", "Johansson", "https://m.media-amazon.com/images/M/MV5BMTM3OTUwMDYwNl5BMl5BanBnXkFtZTcwNTUyNzc3Nw@@._V1_UY317_CR23,0,214,317_AL_.jpg", "1984-11-22", "https://www.imdb.com/name/nm0424060"));
+    // console.log($scope.actors);
 
 
     //**** Presentation Logic ****/
@@ -55,12 +55,27 @@ app.controller("actorCtrl", function ($scope) {
 //       $scope.orderReverse = !$scope.orderReverse;
 //     }
 //   }
-  $scope.class = "";
-  $scope.changeClass = function(){
-    if ($scope.class === "selected")
-      $scope.class = "unselect";
-    else
-      $scope.class = "selected";
-  };
+$scope.selectedActor = null;
+$scope.onSelectActor = function(actor) {
+  if ($scope.selectedActor === actor) {
+    $scope.selectedActor = null;
+  } else {
+    $scope.selectedActor = actor;
+  }
+}
+
+
+  $scope.actors = [];
+  $http.get("actors.json").then(function(res) {
+    // on success
+    for (var i = 0; i < res.data.length; i++) {
+      var actor = new Actor(res.data[i].fname, res.data[i].lname, 
+        res.data[i].image, res.data[i].birthday, res.data[i].imdb);
+      $scope.actors.push(actor);
+      }
+  }, function(err) {
+    console.error(err);
+
+  })
 
 });
